981f4806c5bc1c3c985e01ddc95390a1b1892038
tobon-marco <marco.tobon@email.ucr.edu>
2015-10-30 14:36:06 -0700

    pop-up validation
    have the validation for the popup working

--- application/controllers/post.php
+++ application/controllers/post.php
@@ -15,7 +15,6 @@ class Post extends CI_Controller
 	public function save()
 	{
 		$this->upload_file();
-
 	}
 	private function upload_file()
 	{
@@ -23,7 +22,7 @@ class Post extends CI_Controller
 		$this->form_validation->set_rules('title', 'Title', 'trim|required');
 		$this->form_validation->set_rules('content', 'Content', 'trim');
 
-		if($this->form_validation->run())
+		if($this->form_validation->run() == TRUE)
 		{
 			$type = explode('.', $_FILES["pic_dir"]["name"]);
 			$type = $type[count($type)-1];
@@ -39,65 +38,11 @@ class Post extends CI_Controller
 					}
 				}
 			}
-			else
-			{
-				//PRINT ERROR SINCE IT WAS NOT A VALID IMG FILE	
-			}
 			redirect('profile/');
 		}
-
 		else
-		{
-			echo "ERROR";
-		}
-
+        {
+           
+        }
 	}
-
-
-
-
-/* ========================= WORKING WITHOUT CODEIGNITERS FORM VALIDATION =======================================
-		$filename = "pic_dir";
-
-		if (empty($_POST['title']))
-		{
-			$status = "error";
-			$msg = "Please enter a title";
-		}
-
-		if ($status  != "error")
-		{
-			$config['upload_path'] = 'assets/img/';
-			$config['allowed_types'] = 'gif|jpg|png';
-			$config['max_size'] = 1024 * 8;
-			$config['encrypt_name'] = true;
-
-			$this ->load->library('upload',$config);
-
-			if(!$this->upload->do_upload($filename))
-			{
-				$status = "error";
-				$msg = $this->upload->display_errors('','');
-
-			}
-			else
-			{
-				$this->load->model('file_model');
-				$data = $this->upload->data();
-				$file_id = $this->file_model->insert_file($data['file_name'], $_POST['title'], $_POST['content']);
-				if($file_id)
-				{
-					redirect('Post/index');
-				}
-				else
-				{
-					unlink($data['full_path']);
-					$status = "error";
-					$msg = "Please Try Again";
-				}
-			}
-			@unlink($_FILES[$filename]);
-		}
-		echo json_encode(array('status'=> $status,'msg'=>$msg));
-		*/
 }
--- application/views/template/post_modal.php
+++ application/views/template/post_modal.php
@@ -3,24 +3,78 @@
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
     <h3 id="myModalLabel">Post</h3>
   </div>
-<style> .errors {color: red;} </style>
+<style> 
+    .errors 
+    {
+        color: red;
+        visibility: hidden;
+    } 
+</style>
+
+<script type="text/javascript">
+function Validate()
+{
+    var errtitle = document.getElementById("errtitle");
+    var title = document.getElementById("title");
+    var is_error = false;
+    if(title.value === "")
+    {
+        errtitle.setAttribute("style", "visibility:visible");
+        is_error = true;
+    }
+    var errpic = document.getElementById("errpic");
+    var pic = document.getElementById("pic");
+    var regex =/\.(jpe?g|png|gif|bmp)$/i;
+    //alert(pic.value);
+    if (!(pic.value.match(regex)))
+    {
+        errpic.setAttribute("style", "visibility:visible");
+        is_error = true;
+    }
+    
+    
+    
+    if (is_error)
+    {
+        if(event.preventDefault)
+        {
+            event.preventDefault();
+//             $( "<div>" )
+//                .append( "default " + event.type + " prevented" )
+//                .appendTo( "#log" );
+        }
+        else
+        {
+            event.returnValue = false; // for IE as dont support preventDefault;
+        }   
+        is_error = false;
+    }
+}
+</script>
 
 <div class="modal-body">
   <div class="errors"> <?php echo validation_errors(); ?> </div>
-    <?php echo form_open_multipart("post/save"); ?>
+<!--    <div id="log"></div>-->
+    <?php echo form_open_multipart("post/save", 'id=post_form'); ?>
     <table class="table">
       <tr>
         <td>Title</td>
         <td>
-          <?php echo form_input('title'); ?>
+          <?php echo form_input('title','', 'id=title'); ?>
+        </td>
+        <td id="errtitle" class="errors">
+            Please enter title
         </td>
       </tr>
 
       <tr>
         <td>Picture <i class="icon-upload"></i></td>
         <td>
-          <?php echo form_upload('pic_dir'); ?>
+          <?php echo form_upload('pic_dir', '', 'id=pic'); ?>
           <?php echo $this->input->post('pic_dir'); ?>
+        </td>
+        <td id="errpic" class="errors">
+            Enter picture
         </td>
       </tr>
 
@@ -45,7 +99,7 @@
 </div>
 <div class="modal-footer">
   <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-  <?php echo form_submit('submit', 'Post', 'class="btn btn-primary" id="post_submit"'); ?>
+  <?php echo form_submit('submit', 'Post', 'class="btn btn-primary" id="post_submit"  onclick="Validate();"'); ?>
   <?php echo form_close(); ?>
 </div>
 </div>
