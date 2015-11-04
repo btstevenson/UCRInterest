250b91f3bac7ac4342a1a2353b3902f9d1a8f9fa
John Mark Almaraz <jalma003@ucr.edu>
2015-11-04 14:42:06 -0800

    Revert "Merge branch 'John's-Branch'"
    This reverts commit 0186f5d0d03c496cf9a212b57e1fc1202a209b48, reversing
    changes made to 6fc433c5f9a5010b29dccb60fc20b8917b498488.

--- application/controllers/feed.php
+++ application/controllers/feed.php
@@ -8,12 +8,6 @@ class Feed extends CI_Controller{
 
 	public function index()
 	{
-		$is_logged_in = $this->session->userdata('is_logged_in');
-		if(!isset($is_logged_in) || $is_logged_in !== true)
-		{
-			redirect('user/login');
-		}
-
 		$this->load->view('template/header', $this->data);
 		$this->load->view('template/main_layout', $this->data);
 
@@ -29,29 +23,15 @@ class Feed extends CI_Controller{
 
 		$pins = $this->feed_model->get_pins();
 
-		$this->data = array("this_pid" => $pid, "imgs" => $imgs, "titles" => $titles, "contents" => $contents, "first_name" => $first_name, "last_name" => $last_name, "pins" => $pins);
+		$data = array("this_pid" => $pid, "imgs" => $imgs, "titles" => $titles, "contents" => $contents, "first_name" => $first_name, "last_name" => $last_name, "pins" => $pins);
 
-		$this->load->view('feed_view', $this->data);
+		$this->load->view('feed_view', $data);
 		
 		$this->load->view('template/footer');
 	}
 
 	public function make_pin()
 	{
-		$pid = $this->uri->segment(3);
-		$this->load->model('feed_model');
-		$this->feed_model->make_pin($pid);
-
-		redirect('feed');
-	}
-
-	public function un_pin()
-	{
-		$pid = $this->uri->segment(3);
-		$this->load->model('feed_model');
-		$this->feed_model->un_pin($pid);
-
-		redirect('feed');
-
+		echo "this is a test";
 	}
 }
--- application/views/feed_view.php
+++ application/views/feed_view.php
@@ -3,15 +3,9 @@
 	$new_pid = 0;
 	function make_pin()
 	{
-		redirect('profile');
+
 	}
 ?>
-
-<script>
-	a.thumbnail:hover {
-	    text-decoration: none;
-	}
-</script>
 
     <ul class="thumbnails">
     
@@ -20,42 +14,38 @@
 		?>
 				<div class="row-fliud">
 				 	<li class="span4" display:block>
-						<div class="thumbnail">
-							<div class="caption">
-			            		<img src= "<?php echo base_url($imgs[$i]); ?>"  alt="" >
-			                    <h3><?php echo $titles[$i]; ?></h3>
-			                    <p><?php echo $contents[$i]; ?></p>
-			                    <h6>By: <?php echo $first_name[$i]." ".$last_name[$i] ?></h6>
+						<a class="thumbnail">
 
-		                    <?php
-		                    	$pinned = false;
-			                    for($j = 0; $j < count($pins); $j++)
-			                    {
-			                    	if($pins[$j] == $this_pid[$i])
-			                    	{
-			                    		$pinned = true;
-		                    		}
-		                    	}
-		                    	if($pinned)
+		            		<img src= "<?php echo base_url($imgs[$i]); ?>"  alt="" >
+		                    <h3><?php echo $titles[$i]; ?></h3>
+		                    <p><?php echo $contents[$i]; ?></p>
+		                    <h6>By: <?php echo $first_name[$i]." ".$last_name[$i] ?></h6>
+
+	                    <?php
+	                    	$pinned = false;
+		                    for($j = 0; $j < count($pins); $j++)
+		                    {
+		                    	if($pins[$j] == $this_pid[i])
 		                    	{
-	                		?>
-	                				<p>
-			                    		<a href="feed/un_pin/<?php echo $this_pid[$i] ?>" class="btn btn-block" role="button">unPin!</a>
-		                    		</p>
-	                		<?php
-	                			}
-	                			else
-	                			{
-	        				?>
-		        					<p>
-			                    		<a href="feed/make_pin/<?php echo $this_pid[$i] ?>" class="btn btn-danger btn-block" role="button">Pin!</a>
-			                    	</p>
+		                    		$pinned = true;
+	                    		}
+	                    	}
+	                    	if($pinned)
+	                    	{
+                		?>
+	                    		<input class="btn btn-danger btn-block" type="submit" value="unPin!">
+                		<?php
+                			}
+                			else
+                			{
+                				$new_pid = $this_pid[$i];
+        				?>
+                				<input class="btn btn-block" type="submit" value="Pin!" onclick="make_pin();">
+                		<?php
+                			}
+            			?>
 
-	                		<?php
-	                			}
-	            			?>
-            				</div>
-	            		</div>
+	            		</a>
 					</li>
 				</div>
         <?php
