335b25eaa5c4b99f48e99e4c7924b8ae94ecb60f
John Mark Almaraz <jalma003@ucr.edu>
2015-11-04 16:08:13 -0800

    Fixed conflict with my and Alex's Commits

--- application/views/user/profile_view.php
+++ application/views/user/profile_view.php
@@ -11,43 +11,13 @@
 	</div>
 
 	<div id = "tabs">
-        place holder for likes and such menu
-	</div>
-    <div>
-        <div>
-            <a href="#board_modal" role="button" class="btn btn-danger" data-toggle="modal" id="post_btn">Create Board</a>
-            <?php $this->load->view('template/board_modal'); ?>    
-        </div>
+         <table id="menu">
+            <tr>
+            <td><?php echo anchor('profile/board', 'Board'); ?></td>
+            <td><?php echo anchor('profile/pins', 'Pins'); ?></td>
+            </tr>
+        </table>
     </div>
-
-   <div class="row-fluid">
-        <div class="span4 element"><h2>Board 1</h2><p>A board of a board of a board</p></div>
-        <!--/span-->
-        <div class="span4 element"><h2>Board 2</h2><p>A board that move before</p></div>
-        <!--/span-->
-        <div class="span4 element"><h2>Board 3</h2><p>A board that moves</p></div>
-        <!--/span-->
-    </div>
-
-    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js" type="text/javascript"></script>
-    <script type = "text/javascript">
-        $(".row-fluid").sortable({
-    placeholder: 'span4 placeholder',
-    helper: 'clone',
-    appendTo: 'body',
-    forcePlaceholderSize: true,
-    start: function(event, ui) {
-        $('.row-fluid > div.span4:visible:first').addClass('real-first-child');
-    }, 
-    stop: function(event, ui) {
-        $('.row-fluid > div.real-first-child').removeClass('real-first-child');
-    },
-    change: function(event, ui) {
-        $('.row-fluid > div.real-first-child').removeClass('real-first-child');
-        $('.row-fluid > div.span4:visible:first').addClass('real-first-child');
-    },
-});
-        </script>
 </div>
 
 
