388f9ded35fa11b8d89d8e1f6a260aabd35092ae
tobon-marco <marco.tobon@email.ucr.edu>
2015-12-04 00:39:16 -0800

    --FRIEND NOTIFICATIONS SHOWING UP
    -- minor changes
    -- overall mostly complete

--- application/views/friends_view.php
+++ application/views/friends_view.php
@@ -1,10 +1,63 @@
 <div class ="friends_view">
     <!-- 3 DIVS ONE FOR PENDING ON FOR FRINEDS ONE TO SEARCH-->
-    
     <!-- DIV FOR SEARCHING NEW FRIENDS -->
     <div class="row-fluid">
-        <!--//========= DIV FOR DISPLAYING PENDING REQUESTS FROM USERS/ WILL ALSO INCLUDE NOTIFICATIONS OF NEW ONES ========-->
+        <!--//========= DIV FOR DISPLAYING NOTIFICATIONS  ========-->
+        <div id="notis" class="span8" >
+            <h3>Latest Notifications</h3>
+            <?php
+                if(count($f_notif) == 0)
+                {
+            ?>
+                    <p class="muted">No Recent Notifications.</p>
+            <?php
+                }
+                for ($i = 0; $i < count($f_notif); $i++)
+                {
+                    ?>
+                        <ul style="list-style-type:square">
+                            <?php if($f_notif[$i]->type == "friend_req")
+                            { ?>   
+                            <li><?php echo $u_notif[$i]->first_name." ".$u_notif[$i]->last_name." sent you a friend request" ?> </li>
+                            <?php } 
+                            else { ?>
+                            <li><?php echo $u_notif[$i]->first_name." ".$u_notif[$i]->last_name." accepted your request" ?> </li>
+                            <?php } ?>
+                        </ul>   
+                    <?php
+                }
+            ?>
+        </div> <!-- END NOTIS--> 
+        
+        <div id="new_friends" class="span4" >
+            <div class="navbar navbar-inverse">
+                <div class="navbar-inner">
+                    <div class="container">
 
+                    <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        </a>
+                    <!-- Be sure to leave the brand out there if you want it shown -->
+                        <a class="brand" href="#" >Search for people</a>
+
+                    <!-- Everything you want hidden at 940px or less, place within here -->
+                        <div class="nav-collapse collapse">
+                            <?php echo form_open('friends/search', 'class="navbar-search pull-left"');
+                                echo form_input('Search', '', 'placeholder="Find Friends"'); ?> 
+                            <?php form_close();?>
+                    <!-- .nav, .navbar-search, .navbar-form, etc -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="row-fluid">
+        <!--//========= DIV FOR DISPLAYING PENDING REQUESTS FROM USERS ========-->
         <div id="pending" class="span8" >
             <h3>Pending Requests</h3>
             <?php
@@ -36,34 +89,7 @@
                     <?php
                 }
             ?>
-        </div>
-
-        <div id="new_friends" class="span4" >
-            <div class="navbar navbar-inverse">
-                <div class="navbar-inner">
-                    <div class="container">
-
-                    <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        </a>
-                    <!-- Be sure to leave the brand out there if you want it shown -->
-                        <a class="brand" href="#" >Search for people</a>
-
-                    <!-- Everything you want hidden at 940px or less, place within here -->
-                        <div class="nav-collapse collapse">
-                            <?php echo form_open('friends/search', 'class="navbar-search pull-left"');
-                                echo form_input('Search', '', 'placeholder="Find Friends"'); ?> 
-                            <?php form_close();?>
-                    <!-- .nav, .navbar-search, .navbar-form, etc -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+        </div> <!-- END PENDING-->
     </div> <!-- END ROW -->
     <div class="row-fluid">
         <!-- DIV FOR VIEWING CURRENT FRIENDS -->
